10574fe5de1a7bd4a263cd056dc0347af1205533
André Luiz Pastore Dambros <andreluizpd@gmail.com>
2021-07-04 13:16:26 -0300

    Feat: Added header and simple styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Simple app for tasks and projects</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Simple app for tasks and projects</h1>
+        </Container>
+      </main>
+    </>
   );
 }
 
